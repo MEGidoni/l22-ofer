ac8138f52c221c4456b6c3b439c83660c9ed4cdb
MEGidoni <evi.shimon@gmail.com>
2024-06-25 10:06:04 +0300

    71

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 const init = ()=>{
-document.querySelector("#h1").addEventListener("click",()=>{alert("click_commit2_in gold")})
+document.querySelector("#h1").addEventListener("click",()=>{alert("click_commit2_in gold2171")})
 }
 
 
